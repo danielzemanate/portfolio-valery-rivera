5ab2cbe4f3cfc0c3295221a1011a39aa29f7b4c4
danielzemanate <danielzemanate@gmail.com>
2023-08-25 17:02:00 -0500

    Add images me

--- src/components/main/home/Home.tsx
+++ src/components/main/home/Home.tsx
@@ -1,59 +1,69 @@
-import React from 'react'
+import React from "react";
+import me from "../../../assets/img/me3.jpg";
 
 export const Home = () => {
   return (
     <section className="home section" id="home">
-    <div className="home__container container grid">
+      <div className="home__container container grid">
         <div className="home__content grid">
-            <div className="home__social">
+          <div className="home__social">
+            <a
+              href="https://www.linkedin.com/in/juan-daniel-diaz181997/?locale=en_US"
+              target="_blank"
+              className="home__social-icon"
+            >
+              <i className="uil uil-linkedin-alt"></i>
+            </a>
 
-                <a href="https://www.linkedin.com/in/juan-daniel-diaz181997/?locale=en_US" target="_blank" className="home__social-icon">
-                    <i className="uil uil-linkedin-alt"></i>
-                </a>
-                
-                <a href="https://www.instagram.com/juandaniel.pa/" target="_blank" className="home__social-icon">
-                    <i className="uil uil-instagram"></i>
-                </a>
-                
-                <a href="https://github.com/juandaniel-pa" target="_blank" className="home__social-icon">
-                    <i className="uil uil-github-alt"></i>
-                </a>
-            </div>
-            <div className="home__img">
-                <svg className="home__blob" viewBox="0 0 200 187" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
-                    <mask id="mask0">
-                        <path d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 165.547 
-                        130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 129.362C2.45775 
-                        97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 -0.149132 97.9666 
-                        0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z"/>
-                    </mask>
-                    <g mask="url(#mask0)">
-                        <path d="M190.312 36.4879C206.582 62.1187 201.309 102.826 182.328 134.186C163.346 
-                        165.547 130.807 187.559 100.226 186.353C69.6454 185.297 41.0228 161.023 21.7403 
-                        129.362C2.45775 97.8511 -7.48481 59.1033 6.67581 34.5279C20.9871 10.1032 59.7028 
-                        -0.149132 97.9666 0.00163737C136.23 0.303176 174.193 10.857 190.312 36.4879Z"/>
-                        <image className="home__blob-img" x='-30' y='' href="./assets/images/lookme.jpg"/>
-                    </g>
-                </svg>
-            </div>
-            <div className="home__data">
-                <h1 className="home__title">Hi, I'm Daniel Zemanate</h1>
-                <h3 className="home__subtitle">Full Stack Developer</h3>
-                <p className="home__description">I have experience in web design and development knowledge, producing quality work.</p>
-                <a href="#contact" className="button button--flex">
-                    Contact me <i className="uil uil-telegram-alt button__icon"></i>
-                </a>
+            <a
+              href="https://www.instagram.com/juandaniel.pa/"
+              target="_blank"
+              className="home__social-icon"
+            >
+              <i className="uil uil-instagram"></i>
+            </a>
 
-            </div>
+            <a
+              href="https://github.com/juandaniel-pa"
+              target="_blank"
+              className="home__social-icon"
+            >
+              <i className="uil uil-github-alt"></i>
+            </a>
+          </div>
+          <div className="home__img">
+            <img
+              src={me} // Ajusta la ruta de la imagen según tu estructura de carpetas
+              alt="Huevo al revés"
+              className="egg_image"
+              style={{
+                borderRadius: "50% / 40% 40% 65% 65%",
+                objectFit: "cover", // Esto asegura que la imagen cubra todo el espacio del óvalo
+                width: "230px", // Ajusta el ancho según tus necesidades
+                height: "300px", // Ajusta la altura según tus necesidades
+              }}
+            />
+          </div>
+          <div className="home__data">
+            <h1 className="home__title">Hi, I'm Daniel Zemanate</h1>
+            <h3 className="home__subtitle">Full Stack Developer</h3>
+            <p className="home__description">
+              I have experience in web design and development knowledge,
+              producing quality work.
+            </p>
+            <a href="#contact" className="button button--flex">
+              Contact me <i className="uil uil-telegram-alt button__icon"></i>
+            </a>
+          </div>
         </div>
         <div className="home__scroll">
-            <a href="#about" className="home__scroll-button button--flex">
-                <i className="uil uil-mouse-alt home__scroll-mouse"></i>
-                <span className="home__scroll-name">Scroll down</span>
-                <i className="uil uil-arrow-down home__scroll-arrow"></i>
-            </a>
+          <a href="#about" className="home__scroll-button button--flex">
+            <i className="uil uil-mouse-alt home__scroll-mouse"></i>
+            <span className="home__scroll-name">Scroll down</span>
+            <i className="uil uil-arrow-down home__scroll-arrow"></i>
+          </a>
         </div>
-    </div>
-</section>
-  )
-}
+      </div>
+    </section>
+  );
+};
